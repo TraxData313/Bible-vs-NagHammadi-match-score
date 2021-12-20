351768f769301ed334ee90c31c10e79fe7deb26d
Anton Georgiev <antongeorgiev313@gmail.com>
2021-12-20 20:41:35 +0200

    Update data_prep.py

--- data_prep.py
+++ data_prep.py
@@ -120,7 +120,7 @@ def get_mean_freq_diff(sample_1, sample_2, words_dict):
     freq_diff_mean = df['freq_diff'].mean()
     return freq_diff_mean
     
-def freq_perm_test(parsed_book_1, parsed_book_2, n_tests=10, perms_per_test=100):
+def freq_perm_test(parsed_book_1, parsed_book_2, n_tests=10, perms_per_test=100, do_perm_testing=True, sample_len=False):
     """
     Uses permutation testing to compare the two texts:
     
@@ -136,6 +136,11 @@ def freq_perm_test(parsed_book_1, parsed_book_2, n_tests=10, perms_per_test=100)
     """
     words_dict = get_words_dict([parsed_book_1, parsed_book_2])
     least_len = min(len(parsed_book_1), len(parsed_book_2))
+    if sample_len:
+        if sample_len < least_len:
+            least_len = sample_len
+        else:
+            print(f'WARNING: Input sample_len [{sample_len}] is bigger than least_len [{least_len}]. Using least_len...')
     
     freq_diff_mean_reals_list = []
     freq_diff_mean_perms_list = []
@@ -147,20 +152,22 @@ def freq_perm_test(parsed_book_1, parsed_book_2, n_tests=10, perms_per_test=100)
         sample_2 = random.sample(parsed_book_2, least_len)
         freq_diff_mean_real = get_mean_freq_diff(sample_1, sample_2, words_dict)
         freq_diff_mean_reals_list.append(freq_diff_mean_real)
-
-        # Permutation test:
-        all_samples = sample_1 + sample_2
-        for perm in range(perms_per_test):
-            psample = all_samples.copy()
-            random.shuffle(psample)
-            psample_1 = psample[:int(len(psample)/2)]
-            psample_2 = psample[int(len(psample)/2):]
-            freq_diff_mean_perm = get_mean_freq_diff(psample_1, psample_2, words_dict)
-            freq_diff_mean_perms_list.append(freq_diff_mean_perm)
-            if freq_diff_mean_perm > freq_diff_mean_real:
-                freq_bigger += 1
+        if do_perm_testing:
+            # Permutation test:
+            all_samples = sample_1 + sample_2
+            for perm in range(perms_per_test):
+                psample = all_samples.copy()
+                random.shuffle(psample)
+                psample_1 = psample[:int(len(psample)/2)]
+                psample_2 = psample[int(len(psample)/2):]
+                freq_diff_mean_perm = get_mean_freq_diff(psample_1, psample_2, words_dict)
+                freq_diff_mean_perms_list.append(freq_diff_mean_perm)
+                if freq_diff_mean_perm > freq_diff_mean_real:
+                    freq_bigger += 1
                 
-    zero_hypothesis_prob = freq_bigger/(perms_per_test*n_tests)
-    print(f'Real freq diff: {np.mean(freq_diff_mean_reals_list)}')
-    print(f'Mean perm freq diff: {np.mean(freq_diff_mean_perms_list)}')
-    print(f'Zero hypothesis prob: {zero_hypothesis_prob} ({freq_bigger} out of {perms_per_test*n_tests} tests had bigger freq diffs)')
+    if do_perm_testing:
+        zero_hypothesis_prob = freq_bigger/(perms_per_test*n_tests)
+        print(f'Real freq diff: {np.mean(freq_diff_mean_reals_list)}')
+        print(f'Mean perm freq diff: {np.mean(freq_diff_mean_perms_list)}')
+        print(f'Zero hypothesis prob: {zero_hypothesis_prob} ({freq_bigger} out of {perms_per_test*n_tests} tests had bigger freq diffs)')
+    return np.mean(freq_diff_mean_reals_list)
